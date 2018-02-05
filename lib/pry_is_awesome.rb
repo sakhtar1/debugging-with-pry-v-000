22392dd6058fe6b03218a031bbcd789f0d79d13a
Sharmina Akhtar <sakht318@gmail.com>
2018-02-05 08:44:27 +0000

    automatically backed up by learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -7,7 +7,7 @@ def prying_into_the_method
 	doc = Nokogiri::HTML(open('http://sarwarconsults.com/services/'))
 	services = doc.search("li menu-item-1694'] a['href']").text
 	binding.pry
-	#this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
+	#this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!"
 	#puts this_variable_hasnt_been_interpreted_yet
 end
 
