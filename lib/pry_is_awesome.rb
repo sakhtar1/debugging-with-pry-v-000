e2d981f48da69dffab55ac3ae724b10c3a61d451
Sharmina Akhtar <sakht318@gmail.com>
2018-02-05 08:44:48 +0000

    automatically backed up by learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -5,10 +5,10 @@ def prying_into_the_method
 	#puts inside_the_method
 	#puts "We're about to stop because of pry!"
 	doc = Nokogiri::HTML(open('http://sarwarconsults.com/services/'))
-	services = doc.search("li menu-item-1694'] a['href']").text
+	#services = doc.search("li menu-item-1694'] a['href']").text
 	binding.pry
 	#this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!"
 	#puts this_variable_hasnt_been_interpreted_yet
 end
 
-prying_into_the_method
+#prying_into_the_method
